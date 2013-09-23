779ce9c2a07fee79c80ecfa1a07b9c17eab788e0
Javier Bértoli <javier@netmanagers.com.ar>
2013-09-23 08:06:21 -0300

    Remove lenses own path. It's not working as desired

--- manifests/lens.pp
+++ manifests/lens.pp
@@ -4,22 +4,13 @@ class tcpwrappers::lens {
 
   include tcpwrappers
 
-  file { 'augeas.lenses.dir':
-    ensure  => directory,
-    path    => '/usr/share/augeas/lenses/local',
-    mode    => '0755',
-    owner   => 'root',
-    group   => 'root',
-    require => Package[$::augeas::params::augeas_pkgs],
-  }
-
   file { 'tcpwrappers.lens':
     ensure  => present,
-    path    => '/usr/share/augeas/lenses/local/tcpwrappers.aug',
+    path    => '/usr/share/augeas/lenses/tcpwrappers.aug',
     source  => 'puppet:///modules/tcpwrappers/augeas/tcpwrappers.aug',
     mode    => '0444',
     owner   => 'root',
     group   => 'root',
-    require => File['augeas.lenses.dir'],
+    require => Package[$::augeas::params::augeas_pkgs],
   }
 }
