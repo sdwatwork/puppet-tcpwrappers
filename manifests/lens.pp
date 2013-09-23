b158de8a7718e44aec4371d7855ad3e1c922a39e
Javier Bértoli <javier@netmanagers.com.ar>
2013-09-23 07:42:09 -0300

    Add own dir for local lenses. Rename paths in files

--- manifests/lens.pp
+++ manifests/lens.pp
@@ -1,6 +1,25 @@
+# Class tcpwrappers::lens
+
 class tcpwrappers::lens {
-	file { "/usr/share/augeas/lenses/tcpwrappers.aug":
-		source => "puppet:///modules/tcpwrappers/usr/share/augeas/lenses/tcpwrappers.aug",
-		mode   => 0444;
-	}
+
+  include tcpwrappers
+
+  file { 'augeas.lenses.dir':
+    ensure  => directory,
+    path    => '/usr/share/augeas/lenses/local',
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+    require => Package[$::augeas::params::augeas_pkgs],
+  }
+
+  file { 'tcpwrappers.lens':
+    ensure  => present,
+    path    => '/usr/share/augeas/lenses/local/tcpwrappers.aug',
+    source  => 'puppet:///modules/tcpwrappers/augeas/tcpwrappers.aug',
+    mode    => '0444',
+    owner   => 'root',
+    group   => 'root',
+    require => File['augeas.lenses.dir'],
+  }
 }
